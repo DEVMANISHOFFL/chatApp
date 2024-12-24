504d1175cfbf8e3eaa9e07b00a3d4f66147eb3a4
DEVMANISHOFFl <devmanishofficial@gmail.com>
2024-12-24 22:37:44 +0530

    socket.js optimised

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,38 +1,44 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
 const app = express();
-
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors:{
-        origin:['http://localhost:3000'],
-        methods:['GET', 'POST'],
+    cors: {
+        origin: ['http://localhost:3000'],
+        methods: ['GET', 'POST'],
     },
 });
+
+const userSocketMap = {}; // {userId -> socketId}
+
+let emitOnlineUsersTimeout; // Timeout for emitting online users
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
 }
 
-const userSocketMap = {}; // {userId->socketId}
-
-
-io.on('connection', (socket)=>{
-    const userId = socket.handshake.query.userId
-    if(userId !== undefined){
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId;
+    
+    if (userId !== undefined) {
         userSocketMap[userId] = socket.id;
-    } 
+    }
 
-    io.emit('getOnlineUsers',Object.keys(userSocketMap));
+    // Emit online users list periodically every 5 seconds
+    if (emitOnlineUsersTimeout) {
+        clearTimeout(emitOnlineUsersTimeout);
+    }
+    emitOnlineUsersTimeout = setTimeout(() => {
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    }, 5000); // Emit every 5 seconds
 
-    socket.on('disconnect', ()=>{
+    socket.on('disconnect', () => {
         delete userSocketMap[userId];
-        io.emit('getOnlineUsers',Object.keys(userSocketMap));
-    })
+        io.emit('getOnlineUsers', Object.keys(userSocketMap)); // Emit immediately on disconnect
+    });
+});
 
-})
-
-export {app, io, server};
-
+export { app, io, server };
